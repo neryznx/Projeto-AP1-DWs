7bc859528b37723a4a10146121dc378f68273bbc
neryznx <igor.nery4001@gmail.com>
2024-09-08 20:57:00 -0300

    Update 08/09

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,15 +1,18 @@
-function cadastro(){
+const btnEnviar = document.getElementById('btn-enviar')
+const inputText = document.querySelectorAll('.input-text')
+const span = document.querySelectorAll('.obrigatorio')
 
-    let name = document.getElementById('name').value;
-    let terms = document.getElementById('terms').checked;
+btnEnviar.addEventListener('click', () => {
+    inputText.forEach(function (input, indice) {
+        if (input.value === "") {
+            input.classList.remove('valido')
+            input.classList.add('erro')
+            span[indice].classList.add('campo-obrigatorio')
+        } else {
+            input.classList.remove('erro')
+            input.classList.add('valido')
+            span[indice].classList.remove('campo-obrigatorio')
+        }
+    })
 
-    alert(terms);
-        
-    // if(name){
-    //     alert(name)
-    // }
-    // else{
-    //     alert("O campo Nome é obrigatótio!! ")
-    // }
-
-}
+})
