e3eb2c6b68332eee18bec9fd1d25acd5e53004c9
neryznx <igor.nery4001@gmail.com>
2024-09-08 12:34:20 -0300

    Projeto AP1 DW

---
+++ script/cadastro.js
@@ -0,0 +1,15 @@
+function cadastro(){
+
+    let name = document.getElementById('name').value;
+    let terms = document.getElementById('terms').checked;
+
+    alert(terms);
+        
+    // if(name){
+    //     alert(name)
+    // }
+    // else{
+    //     alert("O campo Nome é obrigatótio!! ")
+    // }
+
+}
